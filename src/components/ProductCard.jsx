1ca097bb34534172426f09ca0d8d20fcccdea26c
jyfhmr <80013819+jyfhmr@users.noreply.github.com>
2024-05-27 15:49:51 -0400

    xdd mandé este commit por algo el 27/05/2024, pero no explicaré por qué

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Link from "next/link";
 function ProductCard({ product }) {
+
+  console.log("xdd")
+
   return (
     <Link href={`/products/${product.id}`} key={product.id} className="bg-white rounded-lg border-gray-800 mb-3">
       <div>{product.id}</div>
